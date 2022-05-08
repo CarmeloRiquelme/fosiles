cf3084bd70756435256edfaa34d2e3738ad98f06
CarmeloRiquelme <105009622+CarmeloRiquelme@users.noreply.github.com>
2022-05-08 17:48:10 +0200

    Update fosilfinder.py

--- fosilfinder.py
+++ fosilfinder.py
@@ -1,12 +1,19 @@
+import streamlit as st
 import cv2
 import numpy as np
+from PIL import Image
 
 while(True):
     url = "http://10.216.4.182:8080/shot.jpg"
     cp = cv2.VideoCapture(url)
-    camera, frame = cap.read()
+    camera, frame = cp.read()
     if frame is not None:
-        cv2.imshow("Frame", frame)
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        im_pil = Image.fromarray(img)
+        
+        #cv2.imshow("Frame", frame) # mostrar tipo cv2
+        st.image(im_pil, caption='Sunrise by the mountains')
+
     q = cv2.waitKey(1)
     if q==ord("q"):
         break
